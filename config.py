51d58be9d0694430a78640801e4d9cedb02ad17a
shamboli <n@shamboli>
2019-12-01 14:14:47 -0600

    updated to v1.0.3

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 USERNAME = "matrix_username"  # Bot's username
 PASSWORD = "matrix_pa$$word"  # Bot's password
 SERVER = "matrix.org"  # Matrix server URL
+GITHUB_TOKEN = "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8" # Github token for posting Gists
